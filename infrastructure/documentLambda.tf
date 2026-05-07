1101e754b8adcfefd53ced979b93422ca41b7dfe
Giancarlo Martinez <60276277+gianmar98@users.noreply.github.com>
2026-05-07 17:20:50 -0500

    added IAM role and policy for Lambda, including S3 (GET, PUT, DELETE) and CloudWatch Logs access to create Log Groups and Streams

--- infrastructure/documentLambda.tf
+++ infrastructure/documentLambda.tf
@@ -1,29 +1,5 @@
-
-resource "aws_iam_role_policy" "DocumentLambdaPolicy" { # what the identity is allowed to do
-  role = aws_iam_role.DocumentLambdaRole.id
-  name = var.document_lambda_policy_name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ],
-        Effect   = "Allow"
-        Resource = "${module.document-s3-bucket.s3_bucket_arn}/*"
-      },
-    ]
-  })
-}
-
-
-
 resource "aws_iam_role" "DocumentLambdaRole" { #the identity (Lambda) itself, with the role attached
-  name                 = var.document_lambda_role_name
-  # permissions_boundary = data.aws_iam_policy.role_boundary.arn #Have not been implemented
+  name = var.document_lambda_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -40,22 +16,34 @@ resource "aws_iam_role" "DocumentLambdaRole" { #the identity (Lambda) itself, wi
   })
 }
 
+resource "aws_iam_role_policy" "DocumentLambdaPolicy" { # what the identity is allowed to do
+  role = aws_iam_role.DocumentLambdaRole.id
+  name = var.document_lambda_policy_name
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      { # S3 Get, Put, and Delete objects from Lambda
+        Sid    = "S3AccessPolicy"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
 
-# {
-# 	"Version": "2012-10-17",
-# 	"Statement": [
-# 		{
-# 			"Sid": "S3GetPutDelete",
-# 			"Effect": "Allow",
-# 			"Action": [
-# 				"s3:GetObject",
-# 				"s3:PutObject",
-# 				"s3:DeleteObject"
-# 			],
-# 			"Resource": [
-# 				"arn:aws:s3:::documentbucket-aci1/*"
-# 			]
-# 		}
-# 	]
-# }
+        Resource = "${module.document-s3-bucket.s3_bucket_arn}/*"
+      },
+      { # Adding CloudWatch Logs to be able to debug Lambda function
+        Sid    = "CloudWatchLogs"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
