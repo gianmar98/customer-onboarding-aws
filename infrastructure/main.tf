98ff4cd4e28b6671a3ed7721c21c970a89fa0554
Giancarlo Martinez <60276277+gianmar98@users.noreply.github.com>
2026-05-09 12:17:39 -0500

    added terraform managed by tag for all resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,8 +12,9 @@ provider "aws" {
   default_tags {
     tags = {
       Project     = var.project_name
-      Environment = var.project_region
+      Environment = var.project_environment
       Owner       = var.project_owner
+      ManagedBy   = "Terraform"
     }
   }
 }
